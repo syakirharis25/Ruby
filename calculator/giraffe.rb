82ec3689e0b7f3ea221641775877a2c5552af059
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-22 00:27:06 +0800

    modified:   calculator/giraffe.rb         modified:   giraffe.rb

--- calculator/giraffe.rb
+++ calculator/giraffe.rb
@@ -1,6 +1,18 @@
-puts "Enter a number: "
-num1 = gets.chomp.to_f
-puts "Enter another number"
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+puts "Enter operator: "
+op = gets.chomp()
+puts "Enter second number: "
 num2 = gets.chomp().to_f
 
-puts (num1 + num2)
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts "invalid operator"
+end
--- giraffe.rb
+++ giraffe.rb
@@ -1,12 +1,18 @@
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+puts "Enter operator: "
+op = gets.chomp()
+puts "Enter second number: "
+num2 = gets.chomp().to_f
 
-def max(num1, num2, num3)
-  if num1 >= num2 and num1 >= num3
-    return num1
-  elsif num2 >= num1 and num2 >= num3
-    return num2
-  else
-    return num3
-  end
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts "invalid operator"
 end
-
-puts max(100, 20, 3)
